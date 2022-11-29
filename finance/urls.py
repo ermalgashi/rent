e080bd6b3345790ef05373b8eb5045305dc85043
Ermalmramori <ermalmramori@gmail.com>
2022-11-29 20:55:26 +0100

    First version for view to exclude overlaping dates in reservation and not allow for resevation in those dates

--- finance/urls.py
+++ finance/urls.py
@@ -6,5 +6,6 @@ app_name = 'reservations'
 urlpatterns = [
     path('', views.reservations_base , name='reservations_base'),
     path('reservations_list/', views.reservations_list , name='reservations_list'),
-
+    path('reservations_detail/<int:pk>/', views.reservations_detail , name='reservations_detail'),
+    
 ]
--- finance/views.py
+++ finance/views.py
@@ -1,9 +1,35 @@
 from django.shortcuts import render
 from .models import Reservation
+from cars.models import Car
+import datetime
 
 # Create your views here.
 def reservations_base(request):
    return render(request,'reservations/reservation_base.html')
 
 def reservations_list(request):
-   return render(request,'reservations/reservation_list.html')
+   reservations = Reservation.objects.all()
+   start_date = '2022-11-24'
+   end_date = '2022-11-26'
+   filter_params = dict(pickup_date__lte=end_date, return_date__gte=start_date) # just for redability
+   is_occupied = Reservation.objects.filter(**filter_params, car__registration_number="01-603-KK").exists()
+   
+   # Code to test from stackoverflow.
+   # for date in date_list: 
+   # ...:     start_date, end_date = date.values() 
+   # ...:     filter_params = dict(booked_for_datetime__date__lte=end_date, booked_till_datetime__date__gte=start_date) 
+   # ...:     is_occupied = Booking.objects.filter(**filter_params, room__name=101).exists() 
+   # ...:     if is_occupied: 
+   # ...:         print('Not Available on this range, start: {} end: {}'.format(start_date, end_date)) 
+   # ...:     else: 
+   # ...:         print('Available on this range, start: {} end: {}'.format(start_date, end_date)) 
+
+
+   return render(request,'reservations/reservation_list.html', {'reservations' : reservations })
+
+def reservations_detail(request, pk):
+    reservation = Reservation.objects.get(pk=pk)
+
+    return render(request, 'reservations/reservation_detail.html', {
+        'reservation': reservation,
+    })
