3a39101cc7f567d73f7a5eea75d58caecc669250
Ermalmramori <ermalmramori@gmail.com>
2022-11-30 13:25:08 +0100

    Made progress in the reservation app filering out booked dates and added the initial parts of reservation_add part

--- finance/urls.py
+++ finance/urls.py
@@ -5,6 +5,7 @@ app_name = 'reservations'
 
 urlpatterns = [
     path('', views.reservations_base , name='reservations_base'),
+    path('reservations_add', views.reservations_add , name='reservations_add'),
     path('reservations_list/', views.reservations_list , name='reservations_list'),
     path('reservations_detail/<int:pk>/', views.reservations_detail , name='reservations_detail'),
     
--- finance/views.py
+++ finance/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Reservation
 from cars.models import Car
+import datetime as dt
 import datetime
 
 # Create your views here.
@@ -9,27 +10,64 @@ def reservations_base(request):
 
 def reservations_list(request):
    reservations = Reservation.objects.all()
-   start_date = '2022-11-24'
-   end_date = '2022-11-26'
-   filter_params = dict(pickup_date__lte=end_date, return_date__gte=start_date) # just for redability
-   is_occupied = Reservation.objects.filter(**filter_params, car__registration_number="01-603-KK").exists()
-   
-   # Code to test from stackoverflow.
-   # for date in date_list: 
-   # ...:     start_date, end_date = date.values() 
-   # ...:     filter_params = dict(booked_for_datetime__date__lte=end_date, booked_till_datetime__date__gte=start_date) 
-   # ...:     is_occupied = Booking.objects.filter(**filter_params, room__name=101).exists() 
-   # ...:     if is_occupied: 
-   # ...:         print('Not Available on this range, start: {} end: {}'.format(start_date, end_date)) 
-   # ...:     else: 
-   # ...:         print('Available on this range, start: {} end: {}'.format(start_date, end_date)) 
 
-
-   return render(request,'reservations/reservation_list.html', {'reservations' : reservations })
+   return render(request,'reservations/reservation_list.html', {'reservations' : reservations})
 
 def reservations_detail(request, pk):
     reservation = Reservation.objects.get(pk=pk)
 
     return render(request, 'reservations/reservation_detail.html', {
+        'reservation': reservation,
+    })
+
+
+
+def reservations_add(request, pk):
+   new_reservation = Reservation.objects.get(pk=pk)
+   reservations = Reservation.objects.filter(car__registration_number=new_reservation.car)
+   start_date = '2022-11-20'
+   end_date = '2022-11-30'
+   filter_params = dict(pickup_date__lte=end_date, return_date__gte=start_date) # just for redability
+   is_occupied = Reservation.objects.filter(**filter_params, car__registration_number="01-603-KK")
+  
+   for reservation in reservations:
+      date_list = [reservation.pickup_date + datetime.timedelta(days=x) for x in range((reservation.return_date-reservation.pickup_date).days)]
+
+      for date in date_list:
+         print(date)
+   
+   #  new_reservation = Reservation.objects.get(pk=1)
+   
+   # reservations = Reservation.objects.filter(car__registration_number=new_reservation.car)
+   # start_date = '2022-11-20'
+   # end_date = '2022-11-30'
+   # filter_params = dict(pickup_date__lte=end_date, return_date__gte=start_date) # just for redability
+   # is_occupied = Reservation.objects.filter(**filter_params, car__registration_number="01-603-KK")
+  
+   # for reservation in reservations:
+   #    date_list = [reservation.pickup_date + datetime.timedelta(days=x) for x in range((reservation.return_date-reservation.pickup_date).days)]
+
+   #    for date in date_list:
+   #       print(date)
+   
+   # date_list = [ 
+   #    dict(start=start_date, end=end_date), 
+   # ]
+
+  
+   # base = dt.datetime.strptime(reservation.pickup_date, "%Y-%m-%d")
+   # end_base = dt.datetime.strptime(reservation.return_date, "%Y-%m-%d")     
+
+   # # Code to test from stackoverflow.
+   # for date in date_list: 
+   #      start_date, end_date = date.values() 
+   #      filter_params = dict(pickup_date__lte=end_date, return_date__gte=start_date)
+   #      is_occupied = Reservation.objects.filter(**filter_params, car__registration_number="01-603-KK").exists() 
+   #      if is_occupied: 
+   #          print('Not Available on this range, start: {} end: {}'.format(start_date, end_date)) 
+   #      else: 
+   #          print('Available on this range, start: {} end: {}'.format(start_date, end_date)) 
+
+   return render(request, 'reservations/reservation_add.html', {
         'reservation': reservation,
     })
